d5c5197874ce8b7bec290ebc302d3f42b45fb5f6
DoKhaiTrinh <trinhdo.khai98@gmail.com>
2023-09-25 08:47:27 +0700

    commit 25/9/2023

--- lib/screen/default/widgets/mainMenu.dart
+++ lib/screen/default/widgets/mainMenu.dart
@@ -21,8 +21,7 @@ class _mainMenuState extends State<mainMenu> {
           Container(
               width: 200,
               padding: const EdgeInsets.all(15),
-              child: Image.asset(
-                  'E:/Work/Android-IOS-web/web_booking/lib/assets/images/hats_logo.png')),
+              child: Image.asset('lib/assets/images/hats_logo.png')),
           Container(
             // width: 1300,
             width: 900,
